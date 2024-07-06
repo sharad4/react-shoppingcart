3945091a82189508ff7eefde6ec0736f4595a828
Sharad Rao <sharad011989@gmail.com>
2024-07-06 16:14:32 +0530

    add

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const CartItem = ({ item, removeFromCart }) => {
+const CartItem = ({ item, updateQuantity, removeFromCart }) => {
   return (
     <div className="border p-2 rounded mb-2 flex justify-between items-center">
       <div>
         <h3 className="font-bold">{item.name}</h3>
         <p>${item.price} x {item.quantity}</p>
+        <div className='flex items-center mt-2'>
+          <button
+            onClick={() => updateQuantity(item, item.quantity - 1)}
+            className='bg-gray-300 text-gray-700 px-2 py-1 rounded mr-2'
+          >
+            -
+          </button>
+          <button
+            onChange={() => updateQuantity(item, item.quantity + 1)}
+            className='bg-gray-300 text-gray-700 px-2 py-1 rounded'
+          >
+            +
+          </button>
+        </div>
       </div>
       <button
         onClick={() => removeFromCart(item)}
